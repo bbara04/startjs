cc48a9a7fc8b0cc13f9096d04a85c0a671819153
Balogh István Barnabás <bdbarni@gmail.com>
2024-04-15 12:20:07 +0200

    added telegram notification

--- run_script.sh
+++ run_script.sh
@@ -11,4 +11,9 @@ echo "$current_datetime" > "$file_path"
 
 echo "A dátum és idő ki lett írva a következő fájlba: $file_path"
 
+TOKEN="5499872795:AAGG2XQ-dbjkGZFIDYPdbvvdJa0XN6WYOHo"
+CHAT_ID="5302671789"
+MESSAGE="Szerver started"
+curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE" > /dev/null
+
 wakeonlan 94:c6:91:17:68:c3
