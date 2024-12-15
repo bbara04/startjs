395da81cc98aca529ee20482bd7eb70da73f947a
bbara04 <bdbarni@gmail.com>
2024-12-15 15:00:01 +0100

    Update sendwol api to use new channel

--- sendwol.sh
+++ sendwol.sh
@@ -11,7 +11,7 @@ fi
 username=$1
 ipaddress=$2
 
-TOKEN="5499872795:AAGG2XQ-dbjkGZFIDYPdbvvdJa0XN6WYOHo"
+TOKEN="7634927453:AAHKziilCwPPLub2iFAUESttmAqqDY4pZPQ"
 CHAT_ID="5302671789"
 MESSAGE="$username started server at $(date +"%H:%M") from $ipaddress"
 curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE" > /dev/null
