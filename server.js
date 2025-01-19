8e1bc8526ae400fbf4cc196a2e0e14b5087f3c42
bbara04 <bdbarni@gmail.com>
2025-01-19 12:31:34 +0100

    Added blacklist

--- server.js
+++ server.js
@@ -7,14 +7,11 @@ import { stat } from 'fs';
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
-
 async function readConfig() {
     try {
         const data = await fs.readFile('config.json', 'utf8');
         const config = JSON.parse(data);
-        return config.ip_address;
+        return config;
     } catch (error) {
         console.error('Error reading config file:', error);
         return null;
@@ -22,7 +19,23 @@ async function readConfig() {
 }
 
 // Replace with the IP or hostname of your remote Linux server
-const host = await readConfig();
+const config = await readConfig();
+const host = config.ipaddress;
+const blacklist = config.blacklist;
+
+
+const ipBlacklistMiddleware = (req, res, next) => {
+    const clientIp = req.ip || req.connection.remoteAddress;
+
+    if (blacklist.includes(clientIp)) {
+        return res.status(403).send('Access denied: Your IP is blacklisted. Contact the administrator for more information.');
+    }
+};
+
+app.use(ipBlacklistMiddleware);
+
+app.use(express.static('public'));
+
 
 // Global variable to store server status and last start time
 let serverStatus = false;
